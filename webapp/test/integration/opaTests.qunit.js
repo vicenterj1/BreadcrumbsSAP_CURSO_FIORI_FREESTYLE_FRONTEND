90007166dec1321f4337ed6b2d079f14b2551bed
vicenterj1 <118080156+vicenterj1@users.noreply.github.com>
2024-11-24 17:45:58 -0300

    Delete webapp directory

--- webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["vicenterj/grandesprojetos/zov/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"vicenterjgrandesprojetos/zov/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
